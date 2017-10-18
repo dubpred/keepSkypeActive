d41518a828fd3a6a66fb7006d8f53a9253b29e48
Bryce Young <pl73775@PP7949C9H2.pncbank.com>
2017-10-18 14:19:18 -0400

    original code made to keep skype from going inactive

---
+++ src/keepAlive/keepActive.java
@@ -0,0 +1,20 @@
+package keepAlive;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+public class keepActive {
+public static void main(String...args) throws AWTException, InterruptedException{
+	Robot r = new Robot();	
+	boolean a = true;
+	do{
+	r.keyPress(KeyEvent.VK_PRINTSCREEN);
+	r.keyRelease(KeyEvent.VK_PRINTSCREEN);
+	TimeUnit.SECONDS.sleep(60);
+	}
+	while(a=true);
+	
+}
+}
